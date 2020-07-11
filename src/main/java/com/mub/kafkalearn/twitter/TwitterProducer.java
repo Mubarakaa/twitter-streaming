13c303e169cee9fa0871afdf09b4a9cf84f6be73
mubaraka <mubaraka@grid-scape.com>
2020-07-11 19:33:36 +0530

    Create safe and throughput consumer

--- src/main/java/com/mub/kafkalearn/twitter/TwitterProducer.java
+++ src/main/java/com/mub/kafkalearn/twitter/TwitterProducer.java
@@ -32,7 +32,6 @@ public class TwitterProducer {
 
     private List<String> terms = Lists.newArrayList("bitcoin", "kafka");
 
-
     public TwitterProducer() {
 
     }
@@ -94,6 +93,17 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create safe producer
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+        // create high throughput producer
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024)); //32 kb
 
         return new KafkaProducer<>(properties);
     }
